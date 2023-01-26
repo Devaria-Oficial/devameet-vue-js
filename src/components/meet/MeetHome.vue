bcc6e2b9a624d3b5e5dba151c84f7448d8e7d5c0
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2023-01-25 22:25:36 -0300

    aula 03 parte 03
    conclusão do principal e ajustes de estilo

---
+++ src/components/meet/MeetHome.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+});
+</script>
+
+<template>
+    <div class="container-principal">
+        <div class="container-meet">
+
+        </div>
+    </div>
+</template>
+
+<style src="@/assets/styles/principal.scss" lang="scss"/>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,16 @@
 import Footer from '@/components/general/Footer.vue';
 import { defineComponent } from 'vue';
 import Header from '../components/general/Header.vue';
+import MeetHome from '../components/meet/MeetHome.vue';
 
 export default defineComponent({
-    components: { Header, Footer }
+    components: { Header, Footer, MeetHome }
 });
 </script>
 
 <template>
     <Header />
+    <MeetHome />
     <Footer />
 </template>
 
